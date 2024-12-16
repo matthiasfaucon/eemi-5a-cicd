3dd6a8279be899bd203f16abd6dffee7108bbf59
Karl MARQUES <karl@kmarques.dev>
2024-12-16 09:53:17 +0100

    delete all routes expect tasks

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const homeController = require("../controllers/homeController");
-const dashboardController = require("../controllers/dashboardController");
-const registerController = require("../controllers/registerController");
-const completedtaskController = require("../controllers/completedtaskController");
+//const homeController = require("../controllers/homeController");
+//const dashboardController = require("../controllers/dashboardController");
+//const registerController = require("../controllers/registerController");
+//const completedtaskController = require("../controllers/completedtaskController");
 const Task = require("../models/task");
 const TaskController = require("../controllers/TaskController");
 
@@ -11,11 +11,11 @@ const TaskController = require("../controllers/TaskController");
 console.log("Router loaded");
 const taskController = new TaskController(Task);
 
-router.get("/", homeController.home);
-router.get("/dashboard", dashboardController.dashboard);
-router.get("/register", registerController.register);
+//router.get("/", homeController.home);
+//router.get("/dashboard", dashboardController.dashboard);
+//router.get("/register", registerController.register);
 router.get("/tasks", taskController.all.bind(taskController));
-router.put("/tasks/:id", taskController.update.bind(taskController));
-router.get("/completedtask", completedtaskController.completedtask);
+//router.put("/tasks/:id", taskController.update.bind(taskController));
+//router.get("/completedtask", completedtaskController.completedtask);
 
 module.exports = router;
